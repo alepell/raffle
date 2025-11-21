54de0b156931a5265da7d1a3a05e5baabcc80767
Alexandre Freire Pellegrino <alesirius@gmail.com>
2025-11-21 17:56:40 -0300

    chore: create raffle and applicant

--- app/Livewire/RaffleApplication.php
+++ app/Livewire/RaffleApplication.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\Applicant;
+use App\Models\Raffle;
 use Livewire\Component;
 
 class RaffleApplication extends Component
@@ -11,6 +13,10 @@ class RaffleApplication extends Component
 
     public function save(): void
     {
+        Applicant::create([
+            'raffle_id' => Raffle::first()->id,
+            'email' => $this->email
+        ]);
         $this->success = true;
     }
 
